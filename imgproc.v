872b0b486468f703d82f9e068cae1ce3a4f4b7df
Ken <ken0x0a+github@gmail.com>
2022-01-21 09:37:55 +0900

    feat: add `resize`

--- imgproc.v
+++ imgproc.v
@@ -11,6 +11,7 @@ fn C.DilateWithParams(&C.Mat, mut &C.Mat, &C.Mat, &C.Point, int, BorderType, ...
 fn C.Canny(&C.Mat, mut &C.Mat, f64, f64)
 fn C.GetStructuringElement(int, C.Size) &C.Mat
 fn C.crop(src &C.Mat, top int, bottom int, left int, right int) &C.Mat
+fn C.Resize(&C.Mat, mut &C.Mat, C.Size, f64, f64, InterpolationFlags) &C.Mat
 
 pub fn (m Mat) crop(top int, bottom int, left int, right int) Mat {
 	return new_mat_from_c(C.crop(m.p, top, bottom, left, right))
@@ -1563,52 +1564,50 @@ pub fn threshold(src Mat, mut dst &Mat, thresh f64, maxvalue f64, typ ThresholdT
 // 	return
 // }
 
-// // InterpolationFlags are bit flags that control the interpolation algorithm
-// // that is used.
-// type InterpolationFlags int
+// InterpolationFlags are bit flags that control the interpolation algorithm
+// that is used.
+enum InterpolationFlags {
+	// InterpolationNearestNeighbor is nearest neighbor. (fast but low quality)
+	nearest_neighbor = 0
 
-// const (
-// 	// InterpolationNearestNeighbor is nearest neighbor. (fast but low quality)
-// 	InterpolationNearestNeighbor InterpolationFlags = 0
+	// InterpolationLinear is bilinear interpolation.
+	linear = 1
 
-// 	// InterpolationLinear is bilinear interpolation.
-// 	InterpolationLinear InterpolationFlags = 1
+	// InterpolationCubic is bicube interpolation.
+	cubic = 2
 
-// 	// InterpolationCubic is bicube interpolation.
-// 	InterpolationCubic InterpolationFlags = 2
+	// InterpolationArea uses pixel area relation. It is preferred for image
+	// decimation as it gives moire-free results.
+	area = 3
 
-// 	// InterpolationArea uses pixel area relation. It is preferred for image
-// 	// decimation as it gives moire-free results.
-// 	InterpolationArea InterpolationFlags = 3
+	// InterpolationLanczos4 is Lanczos interpolation over 8x8 neighborhood.
+	lanczos4 = 4
 
-// 	// InterpolationLanczos4 is Lanczos interpolation over 8x8 neighborhood.
-// 	InterpolationLanczos4 InterpolationFlags = 4
+	// InterpolationDefault is an alias for InterpolationLinear.
+	// default = .linear
 
-// 	// InterpolationDefault is an alias for InterpolationLinear.
-// 	InterpolationDefault = InterpolationLinear
+	// InterpolationMax indicates use maximum interpolation.
+	max = 7
+}
 
-// 	// InterpolationMax indicates use maximum interpolation.
-// 	InterpolationMax InterpolationFlags = 7
-// )
+// Resize resizes an image.
+// It resizes the image src down to or up to the specified size, storing the
+// result in dst. Note that src and dst may be the same image. If you wish to
+// scale by factor, an empty sz may be passed and non-zero fx and fy. Likewise,
+// if you wish to scale to an explicit size, a non-empty sz may be passed with
+// zero for both fx and fy.
+//
+// For further details, please see:
+// https://docs.opencv.org/master/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
+fn resize(src Mat, mut dst &Mat, sz ImagePoint, fx, fy f64, interp InterpolationFlags) {
+	p_size := C.Size{
+		width:  sz.x,
+		height: sz.y,
+	}
 
-// // Resize resizes an image.
-// // It resizes the image src down to or up to the specified size, storing the
-// // result in dst. Note that src and dst may be the same image. If you wish to
-// // scale by factor, an empty sz may be passed and non-zero fx and fy. Likewise,
-// // if you wish to scale to an explicit size, a non-empty sz may be passed with
-// // zero for both fx and fy.
-// //
-// // For further details, please see:
-// // https://docs.opencv.org/master/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
-// func Resize(src Mat, dst *Mat, sz image.Point, fx, fy float64, interp InterpolationFlags) {
-// 	pSize := C.struct_Size{
-// 		width:  C.int(sz.X),
-// 		height: C.int(sz.Y),
-// 	}
-
-// 	C.Resize(src.p, dst.p, pSize, C.double(fx), C.double(fy), C.int(interp))
-// 	return
-// }
+	C.Resize(src.p, mut dst.p, p_size, fx, fy, interp)
+	return
+}
 
 // // GetRectSubPix retrieves a pixel rectangle from an image with sub-pixel accuracy.
 // //
